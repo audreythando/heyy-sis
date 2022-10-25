38c1800c4900419f98a87163041a0b30f3ae1751
audreythando <audreythando@gmail.com>
2022-10-25 10:00:54 +0200

    refactor: added header and sidebar components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
+import Home from './Components/Home';
+import Header from './Components/Header';
+
 
 function App() {
   return (
  <Container>
   <Router>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>} />
     </Routes>
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <Wrapper>
+        <Container>
+
+        </Container>
+    </Wrapper>
+  )
+}
+
+export default Header;
+
+const Wrapper=styled.div``;
+
+const Container=styled.div``;
